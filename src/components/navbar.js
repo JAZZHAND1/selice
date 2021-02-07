faf7d14410a095a6e5997a6a8562731bf0073f47
JAZZHAND1 <47551224+JAZZHAND1@users.noreply.github.com>
2021-02-08 05:11:27 +0600

    Com

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,15 @@
 import react from 'react'
 import inbox from './inbox.png';
+import Image from './Image.png';
 import {AiOutlineStar} from 'react-icons/ai';
 const Navbar = (props)=>{
 
     return (
     <div className="sidenav">
-        <a><img  src={inbox}></img></a>
-        <a><img src={inbox}></img></a>
-        <a><AiOutlineStar color="white" size="1.5em"></AiOutlineStar> </a>
+        <div className="sidenav-div" style={{background:"#34a4eb",borderRadius:"6px"}}><img src={inbox}></img></div>
+        <div className="sidenav-div"><img src={inbox}></img></div>
+        <div className="sidenav-div"><AiOutlineStar color="white" size="2.6em"></AiOutlineStar> </div>
+        <div style={{position:"relative",top:"420px",padding:"30px"}}><img src={Image}></img> </div>
       </div> 
     )
 }
