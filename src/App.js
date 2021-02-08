eb5201d1667e9c601bf9287ddc230f61ec9d012c
JAZZHAND1 <47551224+JAZZHAND1@users.noreply.github.com>
2021-02-08 06:01:43 +0600

    Final

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Inboxcontents from  './components/inboxcard';
 import inbox from './components/inbox.png';
 import Image from './components/Image.png';
 import {AiOutlineStar} from 'react-icons/ai';
+import {HiInformationCircle} from 'react-icons/hi';
+import {FaRegEnvelope} from 'react-icons/fa';
 
 import {FaSearch} from 'react-icons/fa';
 
@@ -25,22 +27,35 @@ function App() {
     </div>
    
 
-</div>
-  
-  <div className="main-nav">
-   <div>
-   <FaSearch className="searchicon"></FaSearch>
-    <input size="1.5em" className="inp">
-   </input>
+ </div>
+   <div className="main-nav">
+    <div>
+      <FaSearch className="searchicon"></FaSearch>
+      <input size="1.5em" className="inp">
+      </input>
    </div>
-   <h2 style={{paddingLeft:"10px"}}>Last CHANCE to get Startup Framework -50% OFF</h2>
-   <hr  style={{position:"absolute",top:"56px",width:"100%",border:"1px solid black"}}></hr>
+   <div className="picker">Move to: 
+     <select style={{borderRadius:"4px",height:"30px",width:"70px",position:"relative",left:"20px",backgroundColor:"#a4a8b0",color:"#f7faff"}}>
+       <option >Trash:</option>
+    </select>
+   </div>
+
+   <h2 style={{paddingLeft:"10px",position:"relative",top:"-10px"}}>Last CHANCE to get Startup Framework -50% OFF  </h2>
+   <hr style={{position:"absolute",top:"80px",width:"100%",border:"1px solid black"}}></hr>
+  
   <AiOutlineStar style={{paddingLeft:"10px",float:"left"}} color="black" size="1.5em"></AiOutlineStar> Designmondo to me 
-  <br></br>
+  <div className="topinfo">24-06-05 12:04 
+  <HiInformationCircle size="2em" style={{position:"relative",top:"10px"}}></HiInformationCircle>
+  <FaRegEnvelope size="2em" style={{position:"relative",top:"10px"}}></FaRegEnvelope>
+  </div>
+
+
+
   <div className="detailbox"> 
   <img className="imgbox" src={Image} style={{position:"relative",top:"170px",left:"560px",margin:"auto"}}></img>
- </div>
+  </div>
 
+  
   <div className="detailbar">
    <h3> Designmodo </h3>
     <h3> Exclusive Deal</h3>
